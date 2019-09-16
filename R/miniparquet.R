53fde3c42cbd7d5f5663afc2aba90ff71e625dcf
Hannes Mühleisen <hannes@muehleisen.org>
2019-09-16 11:37:02 +0200

    converting to data frame

--- R/miniparquet.R
+++ R/miniparquet.R
@@ -1,3 +1,7 @@
 read <- function(f) {
-	.Call(miniparquet_read, f)
+	res <- .Call(miniparquet_read, f)
+	 attr(res, "row.names") <-
+              c(NA_integer_, as.integer(-1 * length(res[[1]])))
+            class(res) <- "data.frame"
+            res
 }
--- src/rwrapper.cpp
+++ src/rwrapper.cpp
@@ -1,6 +1,5 @@
 #include <Rdefines.h>
 
-
 // motherfucker
 #undef error
 #undef length
@@ -28,120 +27,192 @@ static int64_t impala_timestamp_to_nanoseconds(const Int96& impala_timestamp) {
 
 extern "C" {
 
-	SEXP miniparquet_read(SEXP filesxp) {
+SEXP miniparquet_read(SEXP filesxp) {
 
+	if (TYPEOF(filesxp) != STRSXP || LENGTH(filesxp) != 1) {
+		Rf_error("miniparquet_read: Need single filename parameter for query");
+	}
 
-		if (TYPEOF(filesxp) != STRSXP || LENGTH(filesxp) != 1) {
-			Rf_error("miniparquet_read: Need single filename parameter for query");
+	char *fname = (char *) CHAR(STRING_ELT(filesxp, 0));
+
+	auto f = ParquetFile(fname);
+
+	// allocate vectors
+
+	auto ncols = f.columns.size();
+	auto nrows = f.nrow;
+
+	SEXP retlist = PROTECT(NEW_LIST(ncols));
+	if (!retlist) {
+		UNPROTECT(1); // retlist
+		Rf_error("miniparquet_read: Memory allocation failed");
+	}
+	SEXP names = PROTECT(NEW_STRING(ncols));
+	if (!names) {
+		UNPROTECT(2); // retlist, names
+		Rf_error("miniparquet_read: Memory allocation failed");
+	}
+	SET_NAMES(retlist, names);
+	UNPROTECT(1); // names
+
+	for (size_t col_idx = 0; col_idx < ncols; col_idx++) {
+		SEXP varname = PROTECT(
+				mkCharCE(f.columns[col_idx]->name.c_str(), CE_UTF8));
+		if (!varname) {
+			UNPROTECT(2); // varname, retlist
+			Rf_error("miniparquet_read: Memory allocation failed");
 		}
+		SET_STRING_ELT(names, col_idx, varname);
+		UNPROTECT(1); // varname
 
-		char *fname = (char *)CHAR(STRING_ELT(filesxp, 0));
+		SEXP varvalue = NULL;
+		switch (f.columns[col_idx]->type) {
+		case parquet::format::Type::BOOLEAN:
+			varvalue = PROTECT(NEW_LOGICAL(nrows));
+			break;
+		case parquet::format::Type::INT32:
+			varvalue = PROTECT(NEW_INTEGER(nrows));
+			break;
+		case parquet::format::Type::INT64:
+		case parquet::format::Type::DOUBLE:
+		case parquet::format::Type::FLOAT:
+			varvalue = PROTECT(NEW_NUMERIC(nrows));
+			break;
+		case parquet::format::Type::INT96: {
+			varvalue = PROTECT(NEW_NUMERIC(nrows));
+			SEXP cl = PROTECT(NEW_STRING(2));
+			SET_STRING_ELT(cl, 0, PROTECT(mkChar("POSIXct")));
+			SET_STRING_ELT(cl, 1, PROTECT(mkChar("POSIXt")));
+			SET_CLASS(varvalue, cl);
+			setAttrib(varvalue, install("tzone"), PROTECT(mkString("UTC")));
+			UNPROTECT(4);
+			break;
+		}
+		case parquet::format::Type::FIXED_LEN_BYTE_ARRAY:
+		case parquet::format::Type::BYTE_ARRAY:
+			varvalue = PROTECT(NEW_STRING(nrows));
+			break;
+		default:
+			UNPROTECT(1); // retlist
+			Rf_error("miniparquet_read: Unknown column type"); // unlikely
+		}
+		if (!varvalue) {
+			UNPROTECT(2); // varvalue, retlist
+			Rf_error("miniparquet_read: Memory allocation failed");
+		}
+		SET_VECTOR_ELT(retlist, col_idx, varvalue);
+		UNPROTECT(1); /* varvalue */
+	}
 
+	// at this point retlist is fully allocated and the only protected SEXP
 
+	ResultChunk rc;
+	ScanState s;
 
-		auto f = ParquetFile(fname);
+	f.initialize_result(rc);
+	uint64_t dest_offset = 0;
 
-			ResultChunk rc;
-			ScanState s;
+	while (f.scan(s, rc)) {
+		for (size_t col_idx = 0; col_idx < ncols; col_idx++) {
+			auto& col = rc.cols[col_idx];
+			SEXP dest = VECTOR_ELT(retlist, col_idx);
 
-			f.initialize_result(rc);
+			for (uint64_t row_idx = 0; row_idx < rc.nrows; row_idx++) {
+				switch (f.columns[col_idx]->type) {
+				case parquet::format::Type::BOOLEAN:
+					LOGICAL_POINTER(dest)[row_idx + dest_offset] =
+							((bool*) col.data.ptr)[row_idx];
+					break;
+				case parquet::format::Type::INT32:
+					INTEGER_POINTER(dest)[row_idx + dest_offset] =
+							((int32_t*) col.data.ptr)[row_idx];
+					break;
+				case parquet::format::Type::INT64:
+					NUMERIC_POINTER(dest)[row_idx + dest_offset] =
+							(double) ((int64_t*) col.data.ptr)[row_idx];
+					break;
+				case parquet::format::Type::DOUBLE:
+					NUMERIC_POINTER(dest)[row_idx + dest_offset] =
+							((double*) col.data.ptr)[row_idx];
+					break;
+				case parquet::format::Type::FLOAT:
+					NUMERIC_POINTER(dest)[row_idx + dest_offset] =
+							(double) ((float*) col.data.ptr)[row_idx];
+					break;
+				case parquet::format::Type::INT96: {
+					auto val = ((Int96*) col.data.ptr)[row_idx];
+					NUMERIC_POINTER(dest)[row_idx + dest_offset] =
+							impala_timestamp_to_nanoseconds(val) / 1000000000;
 
-			// TODO check for NULLness
-
-			while (f.scan(s, rc)) {
-				for (uint64_t row = 0; row < rc.nrows; row++) {
-					for (auto& col : rc.cols) {
-						switch (col.col->type) {
-						case parquet::format::Type::BOOLEAN:
-							printf("%s",
-									((bool*) col.data.ptr)[row] ? "True" : "False");
-							break;
-
-						case parquet::format::Type::INT32:
-							printf("%d", ((int32_t*) col.data.ptr)[row]);
-							break;
-						case parquet::format::Type::INT64:
-							printf("%lld", ((int64_t*) col.data.ptr)[row]);
-							break;
-						case parquet::format::Type::INT96: {
-							// TODO when is this a timestamp?
-							auto val = ((Int96*) col.data.ptr)[row];
-							time_t a = impala_timestamp_to_nanoseconds(val)
-									/ 1000000000;
-							char buffer[80];
-							strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", gmtime(&a));
-							printf("%s", buffer);
-							break;
-						}
-						case parquet::format::Type::FLOAT:
-							printf("%f", ((float*) col.data.ptr)[row]);
-							break;
-						case parquet::format::Type::DOUBLE:
-							printf("%lf", ((double*) col.data.ptr)[row]);
-							break;
-						case parquet::format::Type::FIXED_LEN_BYTE_ARRAY: {
-							auto& s_ele = col.col->schema_element;
-
-							if (!s_ele->__isset.converted_type) {
-								throw runtime_error("Invalid flba type");
-
-							}
-
-							// TODO what about logical_type??
-							switch (col.col->schema_element->converted_type) {
-							case parquet::format::ConvertedType::DECIMAL: { // this is a giant clusterfuck
-								auto type_len = s_ele->type_length;
-								auto bytes =
-										col.string_heap[((uint64_t*) col.data.ptr)[row]].get();
-								int64_t val = 0;
-								for (auto i = 0; i < type_len; i++) {
-									val = val << ((type_len - i) * 8)
-											| (uint8_t) bytes[i];
-								}
-								printf("%.2f", val / pow(10.0, s_ele->scale));
-
-							}
-								break;
-							default:
-								throw runtime_error("Invalid flba type");
-
-							}
-
-						}
-							break;
-						case parquet::format::Type::BYTE_ARRAY:
-							printf("%s",
-									col.string_heap[((uint64_t*) col.data.ptr)[row]].get());
-
-							break;
-
-						default:
-							throw runtime_error("Invalid type");
-							break;
-
-						}
-						if (col.id < rc.cols.size() - 1) {
-							printf("\t");
-						}
-
-					}
-					printf("\n");
+					break;
+				}
+				 case parquet::format::Type::BYTE_ARRAY: {
+					auto val = col.string_heap[((uint64_t*) col.data.ptr)[row_idx]].get();
+					SET_STRING_ELT(dest, row_idx + dest_offset, mkCharCE(val, CE_UTF8));
+					break;
+				 }
+				default: {
+					auto it = parquet::format::_Type_VALUES_TO_NAMES.find(
+							f.columns[col_idx]->type);
+					UNPROTECT(1); // retlist
+					Rf_error("miniparquet_read: Unknown column type %s",
+							it->second); // unlikely
+				}
 				}
 
 			}
 
+		}
+		dest_offset += rc.nrows;
+
+	}
+	// TODO assert dest_offset = nrows
+
+	assert(dest_offset == nrows);
+	UNPROTECT(1); // retlist
+	return retlist;
+
+	/*
 
 
-	return R_NilValue;
+	 case parquet::format::Type::FIXED_LEN_BYTE_ARRAY: {
+	 auto& s_ele = col.col->schema_element;
+
+	 if (!s_ele->__isset.converted_type) {
+	 throw runtime_error("Invalid flba type");
+
+	 }
+
+	 // TODO what about logical_type??
+	 switch (col.col->schema_element->converted_type) {
+	 case parquet::format::ConvertedType::DECIMAL: { // this is a giant clusterfuck
+	 auto type_len = s_ele->type_length;
+	 auto bytes =
+	 col.string_heap[((uint64_t*) col.data.ptr)[row]].get();
+	 int64_t val = 0;
+	 for (auto i = 0; i < type_len; i++) {
+	 val = val << ((type_len - i) * 8)
+	 | (uint8_t) bytes[i];
+	 }
+	 printf("%.2f", val / pow(10.0, s_ele->scale));
+
+	 }
+	 break;
+	 default:
+	 throw runtime_error("Invalid flba type");
+
+	 }
+
+	 }*/
+
 }
-
 
 // R native routine registration
 #define CALLDEF(name, n)                                                                                               \
 	{ #name, (DL_FUNC)&name, n }
-static const R_CallMethodDef R_CallDef[] = {CALLDEF(miniparquet_read, 1),
+static const R_CallMethodDef R_CallDef[] = { CALLDEF(miniparquet_read, 1),
 
-                                            {NULL, NULL, 0}};
+{ NULL, NULL, 0 } };
 
 void R_init_miniparquet(DllInfo *dll) {
 	R_registerRoutines(dll, NULL, R_CallDef, NULL, NULL);
